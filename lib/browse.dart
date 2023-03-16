80c63bd7e935b433dea9311050395d460765f0fd
BrumaMan <bmazurowski07@gmail.com>
2023-03-16 03:13:28 +0000

    Changed to a more basic UI

--- lib/browse.dart
+++ lib/browse.dart
@@ -4,8 +4,11 @@ import 'package:first_app/authors.dart';
 import 'package:first_app/genres.dart';
 import 'package:first_app/search_result.dart';
 import 'package:first_app/search_result_all.dart';
+import 'package:first_app/util/globals.dart';
+import 'package:first_app/widgets/source_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class Browse extends StatefulWidget {
   const Browse({super.key});
@@ -16,11 +19,8 @@ class Browse extends StatefulWidget {
 
 class _BrowseState extends State<Browse> {
   late TextEditingController textController;
-  List<String> providers = [
-    'All Manga',
-    'Genres',
-    'Authors',
-  ];
+  Box settingsBox = Hive.box('settings');
+  bool search = false;
 
   List<String> backImages = [
     'assets/c5a3090c-4ca0-40a2-9102-e0ee0c6dac15.jpg',
@@ -45,41 +45,24 @@ class _BrowseState extends State<Browse> {
     return Scaffold(
         body: CustomScrollView(
       slivers: [
-        const SliverAppBar(
-          title: Text('Explore'),
-          snap: true,
-          floating: true,
-        ),
-        SliverToBoxAdapter(
-          child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child:
-                  // CupertinoSearchTextField(
-                  //   controller: textController,
-                  //   placeholder: 'Search',
-                  //   placeholderStyle: const TextStyle(color: Colors.white),
-                  //   style: const TextStyle(color: Colors.white),
-                  //   backgroundColor: Colors.grey[700],
-                  //   onSubmitted: (value) {
-                  //     Navigator.of(context).push(CupertinoPageRoute(
-                  //       builder: (context) {
-                  //         return SearchResult(searchTerm: textController.text);
-                  //       },
-                  //     ));
-                  //   },
-                  // ),
-                  Container(
-                clipBehavior: Clip.hardEdge,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
-                child: TextField(
+        SliverAppBar(
+          leading: search
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      search = !search;
+                    });
+                  },
+                  icon: Icon(Icons.arrow_back))
+              : null,
+          title: search
+              ? TextField(
                   controller: textController,
+                  autofocus: true,
                   decoration: InputDecoration(
-                      hintText: "Search",
-                      border: InputBorder.none,
-                      filled: true,
-                      fillColor: Colors.grey,
-                      prefixIcon: Icon(Icons.search)),
+                    hintText: "Search",
+                    border: InputBorder.none,
+                  ),
                   onSubmitted: (value) {
                     Navigator.of(context).push(CupertinoPageRoute(
                       builder: (context) {
@@ -87,97 +70,95 @@ class _BrowseState extends State<Browse> {
                       },
                     ));
                   },
-                ),
-              )),
+                )
+              : Text('Explore'),
+          snap: true,
+          floating: true,
+          actions: [
+            !search
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        search = !search;
+                      });
+                    },
+                    icon: Icon(Icons.search))
+                : SizedBox.shrink(),
+          ],
+        ),
+        // SliverToBoxAdapter(
+        //   child: Padding(
+        //     padding: const EdgeInsets.all(10.0),
+        //     child: Row(
+        //       children: [
+        //         GestureDetector(
+        //           child: Container(
+        //             padding: const EdgeInsets.all(16.0),
+        //             margin: EdgeInsets.only(right: 8.0),
+        //             width: MediaQuery.of(context).size.width / 2 - 12,
+        //             decoration: BoxDecoration(
+        //                 borderRadius: BorderRadius.circular(8.0),
+        //                 color: settingsBox.get('darkMode', defaultValue: false)
+        //                     ? Colors.white.withOpacity(0.1)
+        //                     : Colors.blue.withOpacity(0.08)),
+        //             child: Row(
+        //               children: [Icon(Icons.history), Text(' History')],
+        //             ),
+        //           ),
+        //         ),
+        //         GestureDetector(
+        //           child: Container(
+        //             padding: const EdgeInsets.all(16.0),
+        //             width: MediaQuery.of(context).size.width / 2 - 18,
+        //             decoration: BoxDecoration(
+        //                 borderRadius: BorderRadius.circular(8.0),
+        //                 color: settingsBox.get('darkMode', defaultValue: false)
+        //                     ? Colors.white.withOpacity(0.1)
+        //                     : Colors.blue.withOpacity(0.08)),
+        //             child: Row(
+        //               children: [
+        //                 Icon(Icons.bookmark_border),
+        //                 Text(' Bookmarks')
+        //               ],
+        //             ),
+        //           ),
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: Row(
+              children: [
+                Text('Sources'),
+              ],
+            ),
+          ),
         ),
         SliverList(
             delegate: SliverChildBuilderDelegate(
-          childCount: providers.length,
+          childCount: sources.length,
           (context, index) {
-            return GestureDetector(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                child: Card(
-                  clipBehavior: Clip.hardEdge,
-                  child: Stack(
-                    children: [
-                      Image.asset(
-                          scale: 1.0,
-                          alignment: index != 2
-                              ? Alignment(-1.0, -0.8)
-                              : Alignment(0.0, 0.0),
-                          height: 150.0,
-                          width: 500,
-                          fit: BoxFit.none,
-                          backImages[index]),
-                      Positioned(
-                          bottom: 0.0,
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                    begin: Alignment.bottomCenter,
-                                    end: Alignment.topCenter,
-                                    colors: [
-                                  Colors.black.withOpacity(0.8),
-                                  Colors.black.withOpacity(0.0)
-                                ])
-                                //   boxShadow: [
-                                //   BoxShadow(
-                                //       offset: Offset(0.0, 0.0),
-                                //       spreadRadius: 2.0,
-                                //       blurStyle: BlurStyle.normal,
-                                //       blurRadius: 2.0,
-                                //       color: Colors.black.withOpacity(0.4)),
-                                //   BoxShadow(
-                                //       offset: Offset(0.0, 0.0),
-                                //       spreadRadius: 2.0,
-                                //       blurStyle: BlurStyle.outer,
-                                //       blurRadius: 2.0,
-                                //       color: Colors.black.withOpacity(0.1)),
-                                //   BoxShadow(
-                                //       offset: Offset(0.0, 0.0),
-                                //       spreadRadius: 2.0,
-                                //       blurStyle: BlurStyle.outer,
-                                //       blurRadius: 2.0,
-                                //       color: Colors.black.withOpacity(0.2)),
-                                //   BoxShadow(
-                                //       offset: Offset(0.0, 0.0),
-                                //       spreadRadius: 2.0,
-                                //       blurStyle: BlurStyle.outer,
-                                //       blurRadius: 2.0,
-                                //       color: Colors.black.withOpacity(0.1))
-                                // ],
-                                ),
-                            padding: EdgeInsets.all(12.0),
-                            child: Text(
-                              providers[index],
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20.0),
-                            ),
-                          )),
-                    ],
-                  ),
-                ),
-              ),
+            return ListTile(
+              leading: SourceImage(sourceTitle: sources[index]),
+              title: Text(sources[index]),
+              trailing: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).push(CupertinoPageRoute(
+                        builder: ((context) => SearchResultAll(
+                              name: sources[index],
+                              sort: 'Latest',
+                            ))));
+                  },
+                  child: Text('Latest')),
               onTap: () {
-                if (index == 0) {
-                  Navigator.of(context).push(CupertinoPageRoute(
-                      builder: ((context) => SearchResultAll(
-                            genre: '',
-                            author:
-                                Author(id: '', name: '', bio: '', imageUrl: ''),
-                          ))));
-                } else if (index == 1) {
-                  Navigator.of(context).push(CupertinoPageRoute(
-                      builder: ((context) => GenresScreen())));
-                } else if (index == 2) {
-                  Navigator.of(context).push(CupertinoPageRoute(
-                      builder: ((context) => AuthorsScreen())));
-                }
+                Navigator.of(context).push(CupertinoPageRoute(
+                    builder: ((context) => SearchResultAll(
+                          name: sources[index],
+                          sort: 'Popular',
+                        ))));
               },
             );
           },
